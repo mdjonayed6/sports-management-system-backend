77a98f2272ec9f7d73b388519a4b0e0f7e05053c
mdjonayed6 <jonayed2640@gmail.com>
2024-02-20 17:35:20 +0600

    added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RouterModule } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
   imports: [
-    RouterModule.register([])
-  ],
+    TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '',
+    database: 'sports_management_system',
+    entities: [],
+    synchronize: true
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
